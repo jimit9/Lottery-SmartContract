008866788d60fc402eee0fab0463e42f8a66cc84
jimit <jimit@Jimits-MacBook-Air.local>
2023-05-31 12:00:04 +0530

    second commit

--- lottery.sol
+++ lottery.sol
@@ -12,4 +12,24 @@ contract lottery{
     receive() external payable{
        require( msg.value==1 ether, "Please send 1 Ether");
        participants.push(payable(msg.sender));
+    }function getBalance() public view returns(uint){
+        require(msg.sender==manager,"You are not a manager");
+        return address(this).balance;
     }
+    function random() public view returns(uint) {
+return uint(keccak256(abi.encodePacked(block.prevrandao,block.timestamp)));
+    }
+    function selectWinner() public {
+        require(msg.sender==manager,"You are not a manager");
+        require(participants.length>=3);
+        uint r=random();
+        uint num= r % participants.length;
+        address payable winner;
+        winner = participants[num];
+        winner.transfer(getBalance());
+        participants=new address payable[](0); 
+        // The above line deletes all the participants from the array after declaring the winner.
+
+    }
+   
+}
